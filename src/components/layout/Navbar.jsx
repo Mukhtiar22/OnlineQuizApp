83de1e6ef9f46a105f1772b916f395b15542942b
Mukhtiar22 <mukhtiarali191@gmail.com>
2025-06-15 22:35:32 +0500

    change navbar layout

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,39 +5,29 @@ export default function Navbar() {
   const location = useLocation();
   const isHomePage = location.pathname === "/";
   const isAuthenticated = localStorage.getItem("isAuthenticated");
+  
 
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="logo">
-          QuizMaster
-        </Link>
+        <Link to="/" className="logo">QuizMaster</Link>
         <ul className="nav-links">
-          <li>
-            <Link to="/quizzes">Quizzes</Link>
-          </li>
-          {!isHomePage && (
+          <li><Link to="/quizzes">Quizzes</Link></li>
+          
+          {/* Show Login, Sign Up, Contact only if NOT on Home */}
+          {!isHomePage && !isAuthenticated && (
             <>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/signup">Sign Up</Link>
-              </li>
-              <li>
-            <Link to="/contact">Contact</Link>
-          </li>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/signup">Sign Up</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
             </>
           )}
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-         
 
+          <li><Link to="/about">About</Link></li>
+
+          {/* Show Profile if authenticated */}
           {isAuthenticated && (
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
+            <li><Link to="/profile">Profile</Link></li>
           )}
         </ul>
       </div>
