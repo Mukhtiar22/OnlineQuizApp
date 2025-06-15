b9ba259eba0edc951fd43b4d67735a9c6aab8b38
Mukhtiar22 <mukhtiarali191@gmail.com>
2025-06-15 14:15:54 +0500

    created Pages, setUP internal storgate for the users to save passwords

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Login.css';
+import './login.css';
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState('');
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import './Login.css';
+import './login.css';
 
 export default function Login() {
   const [email, setEmail] = useState('');
