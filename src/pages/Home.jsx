2595260a07f65c6eec6791ecfeb99555d0a2b81b
Mukhtiar22 <mukhtiarali191@gmail.com>
2025-06-16 11:01:27 +0500

    inserting background Imges for better UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,8 @@ export default function Home() {
     <div className="home-container">
       <h1 className="home-title">Welcome to QuizMaster</h1>
       <p className="home-subtitle">Your gateway to fun and challenging quizzes</p>
-      <button>
-        <a href="/quizzes" className="home-button-link">Start Quizzing Now!</a>
+      <button className="home-button">
+        <a href="/quizzes" className="home-button-link">Start Quizzing</a>
       </button>
     </div>
   );
